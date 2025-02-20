840c8925d58f90844e74e66a3926757d016cc3bf
adarshnagrikar14 <adarshnagrikar1404@gmail.com>
2025-02-20 07:02:24 +0530

    feat: add customizable system UI overlay style parameters

--- lib/core/utils/system_chrome_utils.dart
+++ lib/core/utils/system_chrome_utils.dart
@@ -2,12 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 class SystemChromeUtils {
-  static void setSystemUIOverlayStyle({bool isDark = false}) {
+  static void setSystemUIOverlayStyle(
+      {bool isDark = false,
+      Color? statusBarColor,
+      Color? systemNavigationBarColor}) {
     SystemChrome.setSystemUIOverlayStyle(
       SystemUiOverlayStyle(
-        statusBarColor: Colors.transparent,
+        statusBarColor: statusBarColor ?? Colors.transparent,
         statusBarIconBrightness: isDark ? Brightness.light : Brightness.dark,
-        systemNavigationBarColor: Colors.white,
+        systemNavigationBarColor:
+            systemNavigationBarColor ?? const Color(0xFF000000),
         systemNavigationBarIconBrightness: Brightness.dark,
       ),
     );
